b2588ca685ee63aa274f417de7236f21d234a89d
Enrique Rodriguez <enriquerdriguez2.0@gmail.com>
2025-07-11 18:02:18 +0200

    basic app wrapper to run the solution

--- main.rb
+++ main.rb
@@ -1,1 +1,23 @@
-puts 'hello world'
+# frozen_string_literal: true
+
+# Main application class that will wrap the whole app
+class MainApp
+  def self.run
+    new.run
+  end
+
+  def run
+    input_file = ARGV[0]
+
+    if input_file.nil?
+      puts 'Warning: No input file provided'
+      exit 1
+    end
+
+    input_file_content = File.read(input_file)
+    puts input_file_content
+  end
+end
+
+# Run the whole app when the file is executed directly
+MainApp.run if __FILE__ == $PROGRAM_NAME
